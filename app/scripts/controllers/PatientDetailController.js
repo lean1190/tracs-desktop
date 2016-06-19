31af2716a8ca1ab97228a88982da6a49fc5830a7
leito9008 <leito.vm3@hotmail.com>
2016-06-19 18:50:53 -0300

    Se agrego la logica para crear la carpeta de informes si no está y desplegar los documentos en ella

--- app/scripts/controllers/PatientDetailController.js
+++ app/scripts/controllers/PatientDetailController.js
@@ -1,41 +1,66 @@
-(function () {
+(function() {
     "use strict";
 
     angular
         .module("tracsDesktopApp")
         .controller("PatientDetailController", PatientDetailController);
 
-    PatientDetailController.$inject = ["$stateParams", "$http", "$log", "storage", "environment", "GapiHelper"];
+    PatientDetailController.$inject = ["$rootScope", "$q", "$stateParams", "$http", "$log", "storage", "environment", "GapiHelper"];
 
-    function PatientDetailController($stateParams, $http, $log, storage, environment, GapiHelper) {
+    function PatientDetailController($rootScope, $q, $stateParams, $http, $log, storage, environment, GapiHelper) {
 
         var vm = this,
             patientEndpoint = environment.api + "/patient",
-            patientId = $stateParams.id;
+            diagnosisEndpoint = environment.api + "/diagnosis",
+            patientId = $stateParams.id,
+            patientFolderName = GapiHelper.getTracsFolderName();
 
         vm.patient = {};
 
-        function activate() {
-
-            $http.get(patientEndpoint + "/detail/" + patientId).then(function (result) {
-                vm.patient = result.data;
-                storage.setLastVisitedPatient(vm.patient);
-            }, function (error) {
-                $log.error("Ocurrió un error al recuperar el detalle del usuario con id " + patientId, error);
-            });
-
-            /*GapiHelper.createDriveFolder("miCarpetaPiola").then(function () {
-                console.log("### createDriveFolder...");
-                GapiHelper.isFolderCreated("miCarpetaPiola").then(function (res) {
-                    console.log("### isFolderCreated...", res);
-                    if (res.created) {
-                        GapiHelper.getFolderFiles(res.files[0].id).then(function (files) {
-                            console.log("### getFolderFiles...", files);
-                        });
+        /**
+         * Verifica si el usuario tiene en su drive una carpeta
+         * creada con el identificador de la aplicación y el
+         * nombre del paciente. Si no la hay, la crea
+         * @return {promise}  una promesa cuando finalizan las operaciones asincrónicas
+         */
+        function checkAndCreatePatientFolder() {
+            return $q(function(resolve) {
+                GapiHelper.isFolderCreated(patientFolderName).then(function(result) {
+                    // Si no se encontró una carpeta con ese nombre, se crea
+                    if (!result.created) {
+                        GapiHelper.createDriveFolder(patientFolderName).then(resolve);
+                    } else {
+                        resolve(result.files[0]);
                     }
                 });
-            });*/
+            });
+        }
 
+        function activate() {
+            $http.get(patientEndpoint + "/detail/" + patientId).then(function(result) {
+                vm.patient = result.data;
+                storage.setLastVisitedPatient(vm.patient);
+
+                $rootScope.pageTitle = vm.patient.name;
+
+                $http.get(diagnosisEndpoint + "/" + vm.patient.latestDiagnosis).then(function(resultDiagnosis) {
+                    vm.patient.latestDiagnosis = resultDiagnosis.data;
+                }, function(error) {
+                    $log.error("Ocurrió un error al recuperar el diagnóstico del paciente con id " + patientId, error);
+                });
+
+                patientFolderName += " - " + vm.patient.name;
+
+                checkAndCreatePatientFolder().then(function(folder) {
+                    GapiHelper.getFolderFiles(folder.id).then(function(result) {
+                        vm.patient.files = result.files;
+                        console.log("### patient", vm.patient);
+                    });
+                });
+
+            }, function(error) {
+                $log.error("Ocurrió un error al recuperar el detalle del usuario con id " + patientId, error);
+            });
         }
 
         activate();
